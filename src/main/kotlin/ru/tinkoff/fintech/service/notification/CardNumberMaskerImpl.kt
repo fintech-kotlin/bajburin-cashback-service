e1944bbc77e45880e29d36137e3c971756badf7d
Baiburin Valerii <otpt@yandex.ru>
2020-02-24 23:53:57 +0900

    Implement service.notification

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -1,8 +1,21 @@
 package ru.tinkoff.fintech.service.notification
 
+import java.lang.Integer.min
+
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (cardNumber.isEmpty()) return ""
+
+        val length = cardNumber.length
+        if (start > end) throw IndexOutOfBoundsException("Start index cannot be greater than end index")
+
+        val rangeEnd = min(end, length)
+
+        return StringBuilder()
+            .append(cardNumber, 0, start)
+            .append(maskChar.toString().repeat(rangeEnd - start))
+            .append(cardNumber, rangeEnd, length)
+            .toString()
     }
 }
